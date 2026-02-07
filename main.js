7fd689810aabd0c0e144cd8b45568ee638438a6e
Alys-7 <deprez.alais1@gmail.com>
2026-02-07 18:11:30 +0100

    fix: passage de snake case à camel case

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-const nouvelle_partie = document.querySelector('.card-body .btn');
-const stat_erreurs = document.querySelector('#errors');
-const stat_streak = document.querySelector('#record');
+const nouvellePartie = document.querySelector('.card-body .btn');
+const statErreurs = document.querySelector('#errors');
+const statStreak = document.querySelector('#record');
 const pendu = document.querySelector('#hangman');
 const erreur1 = pendu.querySelector('#error-1');
 const erreur2 = pendu.querySelector('#error-2');
 const erreur3 = pendu.querySelector('#error-3');
 const erreur4 = pendu.querySelector('#error-4');
 const erreur5 = pendu.querySelector('#error-5');
-const affichage_mot = document.querySelector('#word-display');
-const conteneur_lettres = document.querySelector('#letters-used');
-const fin_du_jeu = document.querySelector('#game-over-modal');
-const resultat = fin_du_jeu.querySelector('#game-result');
-const rejouer = fin_du_jeu.querySelector('#game-result + div > button');
+const affichageMot = document.querySelector('#word-display');
+const conteneurLettres = document.querySelector('#letters-used');
+const finDuJeu = document.querySelector('#game-over-modal');
+const resultat = finDuJeu.querySelector('#game-result');
+const rejouer = finDuJeu.querySelector('#game-result + div > button');
 
 let mot = "";
 async function chargerMot() {
@@ -34,12 +34,12 @@ async function demarrerJeu() {
 }
 
 function initialiserPendu() {
-    while (affichage_mot.hasChildNodes()) { // On supprime les tirets et lettres de l'ancien mot
-        affichage_mot.removeChild(affichage_mot.firstChild);
+    while (affichageMot.hasChildNodes()) { // On supprime les tirets et lettres de l'ancien mot
+        affichageMot.removeChild(affichageMot.firstChild);
     }
     for (let i=0; i<mot.length; i++) { // On créé de nouveaux tirets correspondant au nouveau mot
         const aAfficher = document.createElement("span");
         aAfficher.classList.add("letter-placeholder");
-        affichage_mot.appendChild(aAfficher);
+        affichageMot.appendChild(aAfficher);
     }
 }
