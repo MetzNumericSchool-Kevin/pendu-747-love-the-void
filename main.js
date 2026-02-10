35c31e025da06914db1393735e322d01414e5361
Alys-7 <deprez.alais1@gmail.com>
2026-02-10 09:08:40 +0100

    bcp de trucs

--- main.js
+++ main.js
@@ -9,8 +9,11 @@ const finDuJeu = document.querySelector('#game-over-modal');
 const resultat = finDuJeu.querySelector('#game-result');
 const rejouer = finDuJeu.querySelector('#game-result + div > button');
 const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-
+const lettresTapees = {correctes: "", toutes: ""};
+let lettresDuMot = [];
+let nombreErreur = 0;
 let mot = "";
+
 async function chargerMot() {
     const response = await fetch("https://trouve-mot.fr/api/random");
     const data = await response.json();
@@ -28,18 +31,9 @@ async function chargerMot() {
 async function demarrerJeu() {
     finDuJeu.close();
     await chargerMot();
-    console.log(mot);
-    const lettresDuMot = [...new Set(mot)].join('');
+    lettresDuMot = [...new Set(mot)].join('');
     console.log(lettresDuMot);
-    let nombreErreur = 0;
-    const lettresTapees = {correctes: "", toutes: ""};
     initialiserPendu();
-    nombreErreur = taperLettre(lettresTapees, nombreErreur);
-    console.log(nombreErreur);
-    // while (lettresTapees.correctes.length != lettresDuMot.length || nombreErreur < 5) {
-    //     nombreErreur = taperLettre(lettresTapees, nombreErreur);
-    // }
-    finDePartie(nombreErreur);
 }
 
 function initialiserPendu() {
@@ -51,32 +45,48 @@ function initialiserPendu() {
         aAfficher.classList.add("letter-placeholder");
         affichageMot.appendChild(aAfficher);
     }
+    nombreErreur = 0; // On initialise le compteur d'erreurs
+    statErreurs.innerHTML = "0/5"; // On initialise le compteur d'erreurs affiché sur la page
+    erreurs.forEach(erreur => {
+        erreur.classList.add('hidden'); // On initialise le pendu
+    });
+    while (conteneurLettres.firstChild) {
+        conteneurLettres.removeChild(conteneurLettres.firstChild); // On initialise le conteneur des lettres tapées
+    }
+    lettresTapees.correctes = ""; // On initialise le dictionnaire des lettres tapées
+    lettresTapees.toutes = "";
 }
 
+demarrerJeu();
+document.addEventListener('keydown', (event) => {
+    nombreErreur = taperLettre(event, lettresTapees, nombreErreur);
+    if (nombreErreur === 5 || lettresTapees.correctes.length === lettresDuMot.length) {
+        finDePartie(nombreErreur);
+    }
+});
+
 function essaiLettre(lettre, nombreErreur, lettresTapees) {
-    const nouvelleLettre = document.createElement("div");
     if (mot.includes(lettre)) {
-        gererReussite(nouvelleLettre);
-        lettresTapees.correctes += nouvelleLettre;
+        gererReussite(lettre);
+        lettresTapees.correctes += lettre;
+        console.log(lettresTapees);
     } else {
-        nombreErreur = gererErreur(nouvelleLettre, nombreErreur);
+        nombreErreur = gererErreur(lettre, nombreErreur);
     }
     return nombreErreur;
 }
 
-function gererReussite(nouvelleLettre) {
-    nouvelleLettre.style = "color: green;";
+function gererReussite(lettre) {
+    const nouvelleLettre = document.createElement("div");
+    nouvelleLettre.textContent = lettre;
+    nouvelleLettre.style.color = "green";
     conteneurLettres.appendChild(nouvelleLettre); // Ajout de la lettre en vert dans le conteneur des lettres déjà essayées
-    for (let i=0; i<mot.length; i++) {
-        if (lettre = mot[i]) {
-            const lettreBienPlacee = affichageMot.childNodes[i];
-            lettreBienPlacee.innerHTML = lettre; // Ecriture de la lettre au bon emplacement
-        }
-    }
 }
 
-function gererErreur(nouvelleLettre, nombreErreur) {
-    nouvelleLettre.style = "color: red;";
+function gererErreur(lettre, nombreErreur) {
+    const nouvelleLettre = document.createElement("div");
+    nouvelleLettre.textContent = lettre;
+    nouvelleLettre.style.color = "red";
     conteneurLettres.appendChild(nouvelleLettre); // Ajout de la lettre en rouge dans le conteneur des lettres déjà essayées
     nombreErreur += 1;
     statErreurs.innerHTML = nombreErreur + "/5";
@@ -88,34 +98,30 @@ function gererErreur(nouvelleLettre, nombreErreur) {
 
 function finDePartie(nombreErreur) {
     if (nombreErreur === 5) {
-        resultat.innerHTML = "Score: " + nombreErreur + "/5" + " Perdu!"
-        statStreak.innerHTML = "0"
+        resultat.innerHTML = "Perdu!" + "<br>Score: " + nombreErreur + "/5";
+        statStreak.innerHTML = "0";
     } else {
-        resultat.innerHTML = "Score: " + nombreErreur + "/5" + " Gagné!"
+        resultat.innerHTML = "Gagné!" + "<br>Score: " + nombreErreur + "/5";
         statStreak.innerHTML = (parseInt(statStreak.textContent) + 1).toString();
     }
     finDuJeu.show();
 }
 
-function taperLettre(lettresTapees, nombreErreur) {
-    document.addEventListener('keydown', function(event) {
-        if (!alphabet.contains(event.key.toUpperCase)) {
-            alert("Veuillez taper une lettre valide.");
-            return nombreErreur;
-        }
-        if (lettresTapees.contains(event.key.toUpperCase)) {
-            alert("Cette lettre a déjà été tapée. Veuillez en choisir une autre.");
-            return nombreErreur;
-        }
-        lettre = event.key.toUpperCase;
-        nombreErreur = essaiLettre(lettre, nombreErreur, lettresTapees);
-        lettresTapees.toutes += lettre;
-    })
+function taperLettre(event, lettresTapees, nombreErreur) {
+    if (!alphabet.includes(event.key.toUpperCase())) {
+        alert("Veuillez taper une lettre valide.");
+        return nombreErreur;
+    }
+    if (lettresTapees.toutes.includes(event.key.toUpperCase())) {
+        alert("Cette lettre a déjà été tapée. Veuillez en choisir une autre.");
+        return nombreErreur;
+    }
+    lettre = event.key.toUpperCase();
+    lettresTapees.toutes += lettre;
+    nombreErreur = essaiLettre(lettre, nombreErreur, lettresTapees);
     return nombreErreur;
 }
 
 // A FAIRE:
-// - Gérer la réussite -> vérifier à chaque essai si le mot est complètement rempli et activer le <dialog> et y écrire les résultats + streak si oui
-// - Gérer l'échec -> si le nombre d'erreurs = 5, activer le <dialog> et y écrire les résultats + streak
 // - Gérer le tiret - dans les mots
-// - Gérer les lettres déjà saisies pour éviter les doublons -> avoir une liste des lettres déjà saisies?
+// - Gérer l'appui de touches utiles (ctrl, flèches, fn, ...)
